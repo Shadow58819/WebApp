b1efe9d319e12bf166383b5709535ca952eefc54
Shadow58819 <shadowofdarknes2009@outlook.de>
2024-11-20 17:49:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ function Passwordsec() {
 function checkPassword() {
 	var password = document.getElementById('password').value;
 	var correctPassword = '1234';
-	var essentialLeonS = 'leon';
-	var LeonM = "leon.m";
-	var alexRR = "alex";
+	var essentialLeonS = 'leons';
+	var dcLeonM = 'leonm';
+		let lmdc = 100;
+	var alexRR = 'alex';
 	if(password === correctPassword) {
 		window.location = "index_2.html";
-	} else if(password == essentialLeonS) {
+	} else if (password == essentialLeonS) {
 		window.location = "https://cdn.modrinth.com/data/k2ZPuTBm/versions/9LqYZE6i/essential_1-3-5-3_fabric_1-21-1.jar";
-	} else if(password == LeonM) {
-		window.location = "";
-	} else if(password == alexRR) {
-		window.location = "https://media.tenor.com/onTlUVMtWy4AAAAM/rickroll-rick.gif";
+	} else if (password == dcLeonM) {
+		while (lmdc > 0) {
+			const link = document.createElement('a');
+			link.href = './res/DaftDeer32.png';
+			link.download = 'DaftDeer32.png';
+			link.click();
+			lmdc--;
+		}
+	} else if (password == alexRR) {
+		window.location = 'https://media.tenor.com/onTlUVMtWy4AAAAM/rickroll-rick.gif';
 	} else {
 		alert("Falches Passwort!\nBitte versuche es erneut.")
 	}
